e57bb3e58449882288c089aa2155cbcdf1d09153
Stuart Geipel <lapimlu@gmail.com>
2025-12-14 01:16:48 -0500

    use new multiply in mul ratio

--- src/geometry/fixed_etc.c
+++ src/geometry/fixed_etc.c
@@ -2,8 +2,6 @@
 
 #include "system/imul.h"
 
-// #include <limits>
-
 #pragma code(code63)
 #pragma data(data63)
 #pragma bss(bss)
@@ -11,21 +9,15 @@
 // stuff that uses fixed integer sizes and doesn't go in the template
 
 geof_t geometry_mul_ratio(geof_t x, geof_t num, unitf_t den, bool *overflow) {
-    *(volatile uint16_t*) 0x2008 = 0x1234;
-    return geof_zero();
-    // int16_t xD = geof_get_raw(x);
-    // int16_t numD = geof_get_raw(num);
-    // uint16_t denD = unitf_get_raw(den);
+    int16_t x_raw = geof_get_raw(x);
+    int16_t num_raw = geof_get_raw(num);
+    uint16_t den_raw = unitf_get_raw(den);
 
     
-    // int64_t firstProd = imul32To64(xD, numD);
-    // int64_t prod = imul32To64(firstProd, denD);
-    // int64_t res = prod >> (8 + 16);
-    // if (res < std::numeric_limits<int16_t>::min() || res > std::numeric_limits<int16_t>::max()) {
-    //     *overflow = true;
-    // }
+    int32_t first_prod = imul16_to_32(x_raw, num_raw);
+    int16_t res = mul_shr24_sat0(first_prod, den_raw, overflow);
 
-    // return geof_from_raw(res);
+    return geof_from_raw(res);
 }
 
 int8_t geometry_round_geof(geof_t x) {
